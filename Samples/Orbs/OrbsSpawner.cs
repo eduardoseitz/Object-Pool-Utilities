2e8ee0be326c49b4eded8c6b207c988e74282578
Eduardo Seitz <eduardoseitz@outlook.com.br>
2026-04-15 21:52:25 -0300

    Small change

--- Samples/Orbs/OrbsSpawner.cs
+++ Samples/Orbs/OrbsSpawner.cs
@@ -15,9 +15,9 @@ public class OrbsSpawner : MonoBehaviour
             Vector2 _mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             
             // Old way to instantiate.
-            //Instantiate(redOrbPrefab, _mousePosition, Quaternion.identity);
-            //Rigidbody2D greenOrbPrefab = Instantiate(greenOrbPrefab, _mousePosition, Quaternion.identity).GetComponent<Rigidbody2D>();
-            //greenOrbPrefab.gravityScale = 1;
+            // Instantiate(redOrbPrefab, _mousePosition, Quaternion.identity);
+            // Rigidbody2D greenOrbPrefab = Instantiate(greenOrbPrefab, _mousePosition, Quaternion.identity).GetComponent<Rigidbody2D>();
+            // greenOrbPrefab.gravityScale = 1;
             
             // Pooled way to instantiate.
             PoolManager.Instance.SpawnObject(redOrbPrefab, new Vector3(_mousePosition.x, _mousePosition.y, 0), Quaternion.identity, (int)ObjectPoolTypes.RedOrb);
